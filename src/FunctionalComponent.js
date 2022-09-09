e28151429db0ab86a54116ccd81b572f7ab7f39f
AlpayGencSoftware <gencoalpay@gmail.com>
2022-09-09 23:40:15 +0300

    useEffect added

--- src/FunctionalComponent.js
+++ src/FunctionalComponent.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const FunctionalComponent = () => {
     const [counter, setCounter]=useState(0);
     //const [isVisible, seIstVisible]= useState(false);
+
+    useEffect(()=>{
+        console.log('useEffect');
+    }, [counter]);
 
     console.log('Functional Component', counter);
     const Increase=()=>{
         setCounter(counter + 1);
     }
     return (
-    <div className='box'>
+    <>
         <h2>Functional Component</h2>
         <p>Counter : {counter}</p>
         <button onClick={()=>Increase()}>Increase</button>
-    </div>
+    </>
   )
 }
 
